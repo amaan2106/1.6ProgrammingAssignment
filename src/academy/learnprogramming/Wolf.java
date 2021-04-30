1000e654a5b454a3bb6f9eaaa1739ba4aa9578a9
amaan2106 <amaan.a2106@gmail.com>
2021-04-30 13:16:28 -0400

    hi

--- src/academy/learnprogramming/Wolf.java
+++ src/academy/learnprogramming/Wolf.java
@@ -1,12 +1,14 @@
 package academy.learnprogramming;
 
 public class Wolf {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        int keyboard;
+        int mouse;
 
-    int keyboard;
-    int mouse;
+        int computer;
 
-int computer;
+        int screen;
 
-    int screen;
-
+    }
 }
